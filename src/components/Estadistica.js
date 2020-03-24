f78ed0c5d3e7be983a38a6a43182f5b38cbb32aa
licaceres <licaceres04@gmail.com>
2020-03-24 17:21:38 -0300

    commit

---
+++ src/components/Estadistica.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Row, Col, Table, Button, Form, Divider, message } from 'antd';
+
+
+
+class Estadistica extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            salas: null
+        }
+    }
+    render(){
+        return{
+            
+        }
+    }
+}
