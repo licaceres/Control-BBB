4b1faeeee85aac3aba2dd0342b8f4ca3c4de08fa
dnosti <48839085+dardonosti@users.noreply.github.com>
2020-03-24 18:13:45 -0300

    [actualización]

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+ 
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { 
+        '@primary-color': '#514563',
+        '@layout-header-background': '#A82582',
+        '@layout-trigger-background': '#7E1B61',
+        '@menu-dark-submenu-bg': '#3F0E30',
+    },
+  }),
+);
