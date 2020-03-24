efb5941165e9ab954a5bb343cff159f61c0ac4fc
dnosti <48839085+dardonosti@users.noreply.github.com>
2020-03-24 16:10:35 -0300

    [actualización]

---
+++ src/components/Estadistica.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Estadistica extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Estadística</h1>
+      </div>
+    );
+  }
+}
+
+export default Estadistica;
