671b52a2a2631069186070636a53c4f6c27e503e
dnosti <48839085+dardonosti@users.noreply.github.com>
2020-03-26 18:55:13 -0300

    [ actualización ]

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import 'antd/dist/antd.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,  
   document.getElementById('root')
 );
   
